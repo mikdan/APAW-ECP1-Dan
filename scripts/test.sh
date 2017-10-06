62bddc9dcd81845a8c2617ac1f491a3a066c426f
danhuang <mikdan89@gmail.com>
2017-10-06 15:55:30 +0200

    first commit

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#echo off
+export workspace=/Users/danhuang/Documents/workspace/APAW-ECP1-Dan
+export PATH=/Users/danhuang/Documents/workspace/apache-maven-3.3.9/bin:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_101.jdk/Contents/Home
+echo -----------------------------------------------
+echo  ". (C) MIW"
+echo -----------------------------------------------
+echo .
+cd $workspace
+echo  "============ mvn clean test (profile: develop)================================"
+echo .
+mvn clean test
+exit
